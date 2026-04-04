8101b90669740b05d68dbef395a4cde739f02109
ZOUHAIR <zouhairmazi@example.com>
2026-04-04 16:08:00 +0100

    msg

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/requests", [AdminController::class, 'index'])->name('requests.index');
+Route::get("/requests/{request}", [AdminController::class, 'show'])->name('requests.show');
